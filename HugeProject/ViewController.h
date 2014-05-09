f5b79a891d15b4daefe55098b2bd5a90ed5c9e91
Anar Enhsaihan <aenhsaihan@gmail.com>
2014-05-09 18:42:59 -0400

    Finished building out the viewController and it’s respective layout.

--- HugeProject/ViewController.h
+++ HugeProject/ViewController.h
@@ -10,4 +10,18 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *previousCurrencyTextField;
+@property (weak, nonatomic) IBOutlet UIButton *previousCurrencyDecrementer;
+@property (weak, nonatomic) IBOutlet UIButton *previousCurrencyIncrementer;
+@property (weak, nonatomic) IBOutlet UIButton *previousCurrencyButton;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *postCurrencyTextField;
+@property (weak, nonatomic) IBOutlet UIButton *postCurrencyDecrementer;
+@property (weak, nonatomic) IBOutlet UIButton *postCurrencyIncrementer;
+@property (weak, nonatomic) IBOutlet UIButton *postCurrencyButton;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *tabulateButton;
+
 @end

0cb951b4e15e76877115aeba9a0a49961bb02a74
Anar Enhsaihan <aenhsaihan@gmail.com>
2014-05-09 17:43:48 -0400

    Initial Commit

---
+++ HugeProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HugeProject
+//
+//  Created by Aditya Narayan on 5/9/14.
+//  Copyright (c) 2014 TurnToTech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ HugeProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HugeProject
+//
+//  Created by Aditya Narayan on 5/9/14.
+//  Copyright (c) 2014 TurnToTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
